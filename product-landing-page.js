07ca963f0321c9ca748a5d8ca7b0c64578d9fe57
Home <elias.norstromm@gmail.com>
2018-06-29 13:11:47 +0100

    fix togglebutton behaviour

--- product-landing-page.js
+++ product-landing-page.js
@@ -3,11 +3,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
   console.log('DOM loaded.');
 });
 
+let maxWidth = matchMedia("(max-width: 700px)");
 let buttonIcon = document.getElementById('nav-icon');
+let burgerIcon = 'fa-bars';
+let crossIcon = 'fa-times';
+
+maxWidth.addListener(function() {
+  buttonIcon.classList.remove(crossIcon);
+});
+
 let toggleIcon = function() {
-  console.log('cake!');
-  this.classList.toggle('fa-times');
-  this.classList.toggle('fa-bars');
-}
+  this.classList.toggle(crossIcon);
+};
 
 buttonIcon.addEventListener('click', toggleIcon);
